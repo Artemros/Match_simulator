efb265b7025b0e8d4d729bb46feb7ff21ab8625c
Artemros <artem.rossolov@gmail.com>
2021-09-22 21:19:32 +0300

    Add files via upload

---
+++ Match_simulator/widget.h
@@ -0,0 +1,34 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include "match.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    void paintEvent(QPaintEvent *) override;
+private:
+    Ui::Widget *ui;
+    QPointF first_Point;
+    QPointF sec_Point;
+    QPointF third_Point;
+    QPointF last_Point;
+    QPointF topRight;
+    QPointF bottomRight;
+
+    Match* match;
+
+    void drawTop(QPainter& painter);
+    void drawStatic(QPainter& painter);
+};
+#endif // WIDGET_H
